0e738e1fec723b2809d5597e983e82c1b65f7288
JudyWu <judyzhaoxinwu@gmail.com>
2015-02-21 12:18:16 -0500

    Comment out Authorization

--- app/models/admin_authorizations.rb
+++ app/models/admin_authorizations.rb
@@ -1,45 +1,45 @@
 class AdminAuthorizations < ActiveAdmin::AuthorizationAdapter
-  def authorized?(action, subject = nil)
-  	if user.researcher
-  		case subject
-  		when normalized(Study)
-  			false
-      when normalized(DataStream)
-        false 
-  		when normalized(AdminUser)
-  			false
-  		else
-  			true
-  		end
-  	else
-  		true
-  	end
-  end
+  # def authorized?(action, subject = nil)
+  # 	if user.researcher
+  # 		case subject
+  # 		when normalized(Study)
+  # 			false
+  #     when normalized(DataStream)
+  #       false 
+  # 		when normalized(AdminUser)
+  # 			false
+  # 		else
+  # 			true
+  # 		end
+  # 	else
+  # 		true
+  # 	end
+  # end
 
-  def scope_collection(collection, action = Auth::READ)
-  	case collection.name
-  	when 'User'
-  	  if user.researcher
-  	  	user.users
-  	  else
-  	  	collection
-  	  end
-  	when 'ActiveAdmin::Comment'
-  	  collection.where(:author_id => user.id)
-    # when 'Study'
-    #   if user.researcher 
-    #     user.studies.name
-    #   else 
-    #     collection
-    #   end
-    # when 'DataStream'
-    #   if user.researcher
-    #      user.data_streams.name
-    #   else 
-    #     collection
-    #   end
-  	else
-  	  collection
-  	end
-  end
+  # def scope_collection(collection, action = Auth::READ)
+  # 	case collection.name
+  # 	when 'User'
+  # 	  if user.researcher
+  # 	  	user.users
+  # 	  else
+  # 	  	collection
+  # 	  end
+  # 	when 'ActiveAdmin::Comment'
+  # 	  collection.where(:author_id => user.id)
+  #   # when 'Study'
+  #   #   if user.researcher 
+  #   #     user.studies.name
+  #   #   else 
+  #   #     collection
+  #   #   end
+  #   # when 'DataStream'
+  #   #   if user.researcher
+  #   #      user.data_streams.name
+  #   #   else 
+  #   #     collection
+  #   #   end
+  # 	else
+  # 	  collection
+  # 	end
+  # end
 end
