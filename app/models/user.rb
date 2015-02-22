34816b563fe003bcf37ba1f7321cfd072ecb4877
JudyWu <judyzhaoxinwu@gmail.com>
2015-02-22 00:50:52 -0500

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -288,11 +288,11 @@ class User < ActiveRecord::Base
     else 
       all_calendar_data_points.each do |data_point| 
         json_data[:users][:c6651b99_8f9c_4d83_8f4b_8c02a00ddf9c][:daily][data_point.body.date + 'T00:00:00.000Z'] = {
-          max_gait_speed_in_meter_per_second: data_point.body.max_gait_speed_in_meter_per_second.blank? ? nil : data_point.body.max_gait_speed_in_meter_per_second,
+          max_gait_speed_in_meter_per_second:  data_point.body.max_gait_speed_in_meter_per_second,
           # data_point.body.max_gait_speed_in_meter_per_second,
-          active_time_in_seconds: data_point.body.active_time_in_seconds.blank? ? nil : data_point.body.active_time_in_seconds,
+          active_time_in_seconds: data_point.body.active_time_in_seconds,
           # data_point.body.active_time_in_seconds,
-          time_not_at_home_in_seconds:  data_point.body.time_not_at_home_in_seconds.blank? ? nil : data_point.body.time_not_at_home_in_seconds
+          time_not_at_home_in_seconds:  data_point.body.time_not_at_home_in_seconds
           # data_point.body.time_not_at_home_in_seconds
         }   
       end
