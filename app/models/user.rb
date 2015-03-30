12666edfc1d750a1a0b5a8c5be50fa3645dbe8d7
Judy Wu <judyzhaoxinwu@gmail.com>
2015-03-30 09:57:47 -0400

    Debug calendar view data json

--- app/models/user.rb
+++ app/models/user.rb
@@ -324,7 +324,7 @@ class User < ActiveRecord::Base
     else 
       all_calendar_data_points.each do |data_point| 
         json_data[:users][:c6651b99_8f9c_4d83_8f4b_8c02a00ddf9c][:daily][data_point.body.date + 'T00:00:00.000Z'] = {
-          max_gait_speed_in_meter_per_second:  data_point.body.max_gait_speed_in_meter_per_second,
+          max_gait_speed_in_meter_per_second: data_point.body.max_gait_speed_in_meter_per_second.round,
           active_time_in_seconds: data_point.body.active_time_in_seconds,
           time_not_at_home_in_seconds:  data_point.body.time_not_at_home_in_seconds
           
@@ -350,7 +350,5 @@ class User < ActiveRecord::Base
     data.body.data.nil? ? nil : data.body.data.send(attribute)
   end 
 
-  def escape_nil_body(data, attribute)
-    data.body.nil? ? nil : data.body.send(attribute)
-  end 
+  
 end
