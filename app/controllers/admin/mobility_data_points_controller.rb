d376e8466377ab53827046e6dd4b565ecbb7be31
Judy Wu <judyzhaoxinwu@gmail.com>
2015-03-30 22:06:47 -0400

    Put all data points controllers into admin folder

---
+++ app/controllers/admin/mobility_data_points_controller.rb
@@ -0,0 +1,8 @@
+class Admin::MobilityDataPointsController < ApplicationController
+  def index
+  	@user = User.find(params[:user_id])
+  	respond_to do |format|
+      format.csv {render text: @user.mobility_data_csv }
+    end
+  end 
+end
