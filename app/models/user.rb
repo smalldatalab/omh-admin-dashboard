a0e4627fcaf438220b241577c95a458348337f37
Judy Wu <judyzhaoxinwu@gmail.com>
2015-04-03 06:09:17 -0400

    Debugging for ohmage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,140 +1,140 @@
-class User < ActiveRecord::Base 
+class User < ActiveRecord::Base
   # after_create :check_data_points
 
   has_many :study_participants
   has_many :studies, through: :study_participants
 
   has_many :admin_users, through: :studies
-  
-  has_many :surveys, through: :studies 
+
+  has_many :surveys, through: :studies
   has_many :data_streams, through: :studies
-  
+
 
   accepts_nested_attributes_for :studies
-  
+
   validates :studies, :gmail, presence: true
   validates_format_of :gmail, without: /\s/, message: "can't have space"
   validates_uniqueness_of :gmail
   # validates_acceptance_of :gmail, message: "This gmail address doesn't have any data points", :if => Proc.new { |user| user.gmail.nil? ? nil : PamUser.where('email_address' => {'address' => user.gmail.gsub(/\s+/, "").downcase}).blank? }
 
-  def registrated_in_database 
-    if PamUser.where('email_address' => {'address' => self.gmail.gsub(/\s+/, "").downcase}).blank? 
+  def registrated_in_database
+    if PamUser.where('email_address' => {'address' => self.gmail.gsub(/\s+/, "").downcase}).blank?
       return "Yes"
-    else 
+    else
       return "No"
-    end 
+    end
   end
 
   def user_record
-    if PamUser.where('email_address' => {'address' => self.gmail.gsub(/\s+/, "").downcase}).blank? 
+    if PamUser.where('email_address' => {'address' => self.gmail.gsub(/\s+/, "").downcase}).blank?
       return nil
-    else 
+    else
       return PamUser.find_by('email_address' => {'address' => self.gmail.gsub(/\s+/, "").downcase})
-    end 
+    end
   end
 
   def most_recent_data_point_date(data_stream)
-    if user_record.nil? 
+    if user_record.nil?
       return ''
-    else  
-      if user_record.pam_data_points.where('header.schema_id.name' => data_stream).last.nil? 
-        return '' 
-      else 
+    else
+      if user_record.pam_data_points.where('header.schema_id.name' => data_stream).last.nil?
+        return ''
+      else
         DateTime.parse(user_record.pam_data_points.where('header.schema_id.name' => data_stream).order('header.creation_date_time_epoch_milli DESC').limit(1).first.header.creation_date_time).to_formatted_s(:long_ordinal)
-      end 
+      end
     end
-  end 
+  end
 
-  def most_recent_ohmage_data_point_date 
-    if user_record.nil? 
+  def most_recent_ohmage_data_point_date
+    if user_record.nil?
       return ''
-    else  
-      if user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).last.nil? 
-        return '' 
-      else 
+    else
+      if user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).last.nil?
+        return ''
+      else
         DateTime.parse(user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).order('header.creation_date_time_epoch_milli DESC').limit(1).first.header.creation_date_time).to_formatted_s(:long_ordinal)
-      end 
+      end
     end
-  end 
+  end
 
   def all_pam_data_points
-    if user_record.nil? 
-      return nil 
-    else 
-      if user_record.pam_data_points.where('header.schema_id.name' => 'photographic-affect-meter-scores').last.nil? 
-        return nil 
-      else 
+    if user_record.nil?
+      return nil
+    else
+      if user_record.pam_data_points.where('header.schema_id.name' => 'photographic-affect-meter-scores').last.nil?
+        return nil
+      else
         user_record.pam_data_points.where('header.schema_id.name' => 'photographic-affect-meter-scores')
       end
     end
-  end 
+  end
 
   def all_mobility_data_points
-    if user_record.nil? 
-      return nil 
-    else 
-      if user_record.pam_data_points.where('header.schema_id.name' => 'mobility-stream-iOS').last.nil? 
-        return nil 
-      else 
+    if user_record.nil?
+      return nil
+    else
+      if user_record.pam_data_points.where('header.schema_id.name' => 'mobility-stream-iOS').last.nil?
+        return nil
+      else
         user_record.pam_data_points.where('header.schema_id.name' => 'mobility-stream-iOS')
       end
     end
-  end 
+  end
 
   def all_ohmage_data_points
-    if user_record.nil? 
-      return nil 
-    else 
-      if user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).last.nil? 
-        return nil 
-      else 
+    if user_record.nil?
+      return nil
+    else
+      if user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).last.nil?
+        return nil
+      else
         user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/)
       end
     end
-  end 
+  end
 
   def all_calendar_data_points
-    if user_record.nil? 
-      return nil 
-    else 
-      if user_record.pam_data_points.where('header.schema_id.name' => 'mobility-daily-summary').last.nil? 
-        return nil 
-      else 
+    if user_record.nil?
+      return nil
+    else
+      if user_record.pam_data_points.where('header.schema_id.name' => 'mobility-daily-summary').last.nil?
+        return nil
+      else
         user_record.pam_data_points.where('header.schema_id.name' => 'mobility-daily-summary')
 
       end
     end
-  end 
-  
+  end
+
 
   def pam_data_csv
     CSV.generate do |csv|
       csv << [
               'id',
               'class',
-              'user id', 
+              'user id',
               'name space',
-              'name', 
-              'version major', 
-              'version minor', 
-              'creation date time', 
+              'name',
+              'version major',
+              'version minor',
+              'creation date time',
               'source name',
               'modality',
               'affect arousal',
               'negative affect',
               'positive affect',
-              'effective time frame date time', 
+              'effective time frame date time',
               'affect valence',
               'mood'
              ]
         if all_pam_data_points.nil?
           return nil
-        else  
+        else
           all_pam_data_points.each do |data_point|
             csv << [
                   data_point._id,
                   data_point._class,
-                  data_point.user_id, 
+                  data_point.user_id,
                   data_point.header.schema_id.namespace,
                   data_point.header.schema_id.name,
                   data_point.header.schema_id.version.major,
@@ -148,7 +148,7 @@ class User < ActiveRecord::Base
                   data_point.body.effective_time_frame.nil? ? nil : data_point.body.effective_time_frame.date_time,
                   escape_nil_body(data_point, :affect_valence),
                   escape_nil_body(data_point, :mood)
-                 ]         
+                 ]
         end
       end
     end
@@ -159,32 +159,32 @@ class User < ActiveRecord::Base
       csv << [
               'id',
               'class',
-              'user id', 
+              'user id',
               'name space',
-              'name', 
-              'version major', 
-              'version minor', 
-              'creation date time', 
+              'name',
+              'version major',
+              'version minor',
+              'creation date time',
               'source name',
               'modality',
               'activity',
               'confidence',
               'speed',
-              'longitude', 
+              'longitude',
               'bearing',
               'latitude',
               'horizontal accuracy',
-              'vertical accuracy', 
-              'altitude' 
+              'vertical accuracy',
+              'altitude'
              ]
       if all_mobility_data_points.nil?
-        return nil 
-      else 
+        return nil
+      else
         all_mobility_data_points.each do |data_point|
           csv << [
                 data_point._id,
                 data_point._class,
-                data_point.user_id, 
+                data_point.user_id,
                 data_point.header.schema_id.namespace,
                 data_point.header.schema_id.name,
                 data_point.header.schema_id.version.major,
@@ -195,33 +195,35 @@ class User < ActiveRecord::Base
                 escape_nil_activities(data_point, 'activity'),
                 escape_nil_activities(data_point, 'confidence'),
                 escape_nil_location(data_point, :speed),
-                escape_nil_location(data_point, :longitude), 
+                escape_nil_location(data_point, :longitude),
                 escape_nil_location(data_point, :bearing),
-                escape_nil_location(data_point, :latitude), 
-                escape_nil_location(data_point, :horizontal_accuracy), 
+                escape_nil_location(data_point, :latitude),
+                escape_nil_location(data_point, :horizontal_accuracy),
                 escape_nil_location(data_point, :vertical_accuracy),
                 escape_nil_location(data_point, :altitude)
-               ] 
+               ]
         end
       end
     end
   end
 
-  def get_all_survey_question_keys 
-    if user_record.nil? 
+  def get_all_survey_question_keys
+    if user_record.nil?
       return nil
     else
-      if user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).last.nil? 
+      if user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).last.nil?
         return nil
-      else 
+      else
         survey_keys = []
-        user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).body.each do |key, value| 
-          survey_keys.push(key) unless survey_keys.include? key 
+        user_record.pam_data_points.where('header.acquisition_provenance.source_name' => /^Ohmage/).each do |a|
+          a.body.data do |key, value|
+            survey_keys.push(key) unless survey_keys.include? key
+          end
         end
         return survey_keys
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
   def ohmage_data_csv
     CSV.generate do |csv|
@@ -229,21 +231,21 @@ class User < ActiveRecord::Base
       if keys
         csv << keys
         if all_ohmage_data_points.nil?
-          return nil 
-        else 
+          return nil
+        else
           all_ohmage_data_points.each do |data_point|
             csv << get_all_survey_question_values(keys, data_point)
           end
         end
       end
-    end 
+    end
   end
 
   def get_all_survey_question_values(survey_keys, data_point)
     survey_values = []
-    data_point.body.each do |key, value|
+    data_point.body.data do |key, value|
       survey_values.push(survey_keys.include?(key) ? value : '')
-    end 
+    end
   end
 
   def mobility_daily_summary_data_csv
@@ -252,83 +254,83 @@ class User < ActiveRecord::Base
               'id',
               'user id',
               'class',
-              'namespace', 
+              'namespace',
               'name',
               'version major',
               'version minor',
-              'creation date time', 
-              'creation date time epoch milli', 
-              'source_name', 
+              'creation date time',
+              'creation date time epoch milli',
+              'source_name',
               'modality',
-              'date', 
+              'date',
               'device',
               'active time in seconds',
-              'walking distance in km', 
+              'walking distance in km',
               'geodiameter in km',
-              'max gait speed in meter per second', 
+              'max gait speed in meter per second',
               'leaving home time',
-              'return home time', 
+              'return home time',
               'time not at home in seconds',
               'coverage'
              ]
       if all_calendar_data_points.nil?
-        return nil 
-      else 
+        return nil
+      else
         all_calendar_data_points.each do |data_point|
           csv << [
                   data_point._id,
-                  data_point.user_id, 
+                  data_point.user_id,
                   data_point._class,
                   data_point.header.schema_id.namespace,
                   data_point.header.schema_id.name,
                   data_point.header.schema_id.version.major,
                   data_point.header.schema_id.version.minor,
                   data_point.header.creation_date_time,
-                  data_point.header.creation_date_time_epoch_milli, 
+                  data_point.header.creation_date_time_epoch_milli,
                   data_point.header.acquisition_provenance.source_name,
                   data_point.header.acquisition_provenance.modality,
                   escape_nil_body(data_point, :date),
                   escape_nil_body(data_point, :device),
                   escape_nil_body(data_point, :active_time_in_seconds),
-                  escape_nil_body(data_point, :walking_distance_in_km), 
-                  escape_nil_body(data_point, :geodiameter_in_km), 
+                  escape_nil_body(data_point, :walking_distance_in_km),
+                  escape_nil_body(data_point, :geodiameter_in_km),
                   escape_nil_body(data_point, :max_gait_speed_in_meter_per_second),
                   escape_nil_body(data_point, :leave_home_time),
-                  escape_nil_body(data_point, :return_home_time), 
+                  escape_nil_body(data_point, :return_home_time),
                   escape_nil_body(data_point, :time_not_at_home_in_seconds),
                   escape_nil_body(data_point, :coverage)
-                  ] 
+                  ]
         end
       end
     end
-  end 
+  end
 
 
-  def calendar_data_json 
-    json_data = { 
+  def calendar_data_json
+    json_data = {
                   users: {
                     c6651b99_8f9c_4d83_8f4b_8c02a00ddf9c: {
                       fullname: "",
                         givenname: "",
                         familyname: "",
                         daily: {}
-                    }                  
+                    }
                   }
                 }
-    if all_calendar_data_points.nil? 
-      return nil 
-    else 
-      all_calendar_data_points.each do |data_point| 
+    if all_calendar_data_points.nil?
+      return nil
+    else
+      all_calendar_data_points.each do |data_point|
         json_data[:users][:c6651b99_8f9c_4d83_8f4b_8c02a00ddf9c][:daily][data_point.body.date + 'T00:00:00.000Z'] = {
           max_gait_speed_in_meter_per_second: escape_and_round(data_point.body.max_gait_speed_in_meter_per_second),
           active_time_in_seconds: escape_and_round(data_point.body.active_time_in_seconds),
           time_not_at_home_in_seconds:  escape_and_round(data_point.body.time_not_at_home_in_seconds)
-          
-        }   
+
+        }
       end
     end
     return JSON.parse(json_data.to_json)
-  end  
+  end
 
   def escape_and_round(data)
     data ? data.round : 0
@@ -337,10 +339,10 @@ class User < ActiveRecord::Base
   def escape_nil_location(data, attribute)
     data.body.location.nil? ? nil : data.body.location.send(attribute)
   end
-  
+
   def escape_nil_home(data, attribute)
     data.body.home.nil? ? nil : data.body.home.send(attribute)
-  end 
+  end
 
   def escape_nil_activities(data, attribute)
     data.body.activities.nil? ? nil : data.body.activities[0][attribute]
@@ -348,10 +350,10 @@ class User < ActiveRecord::Base
 
   def escape_nil_data(data, attribute)
     data.body.data.nil? ? nil : data.body.data.send(attribute)
-  end 
+  end
 
   def escape_nil_body(data, attribute)
     data.body.nil? ? nil : data.body.send(attribute)
-  end 
-  
+  end
+
 end
