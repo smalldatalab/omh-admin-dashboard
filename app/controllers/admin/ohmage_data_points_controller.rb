009f23ac13ee2d7b599ec090aa327082befbeae4
Judy Wu <judyzhaoxinwu@gmail.com>
2015-05-04 14:10:31 -0400

    Add admin_id to ohmage_csv

--- app/controllers/admin/ohmage_data_points_controller.rb
+++ app/controllers/admin/ohmage_data_points_controller.rb
@@ -1,10 +1,8 @@
 class Admin::OhmageDataPointsController < ApplicationController
-
   def index
     @user   = User.find(params[:user_id])
     respond_to do |format|
-       format.csv {render text: @user.ohmage_data_csv(current_admin_user.id) }
+      format.csv {render text: @user.ohmage_data_csv(current_admin_user.id) }
     end
   end
-
 end
