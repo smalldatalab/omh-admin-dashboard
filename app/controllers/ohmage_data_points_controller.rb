357e6599a8d5bc745e6722422601c8c26164dd65
Judy Wu <judyzhaoxinwu@gmail.com>
2015-05-06 12:35:32 -0400

    ohmage_data_points_controller

--- app/controllers/ohmage_data_points_controller.rb
+++ app/controllers/ohmage_data_points_controller.rb
@@ -1,9 +1,9 @@
 class Admin::OhmageDataPointsController < ApplicationController
 
   def index
-    @user = User.find(params[:user_id])
+    @user   = User.find(params[:user_id])
     respond_to do |format|
-       format.csv {render text: @user.ohmage_data_csv }
+      format.csv {render text: @user.ohmage_data_csv(current_admin_user.id)}
     end
   end
 end
