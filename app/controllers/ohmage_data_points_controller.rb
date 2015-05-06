0617945261260b7e39d35e68257ecf4c31971cca
Judy Wu <judyzhaoxinwu@gmail.com>
2015-05-06 12:38:37 -0400

    Delete application controller from admin file

--- app/controllers/ohmage_data_points_controller.rb
+++ app/controllers/ohmage_data_points_controller.rb
@@ -1,5 +1,4 @@
-class Admin::OhmageDataPointsController < ApplicationController
-
+class OhmageDataPointsController < ApplicationController
   def index
     @user   = User.find(params[:user_id])
     respond_to do |format|
