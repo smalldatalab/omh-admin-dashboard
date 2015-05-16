5ea64e71a32977ec4a735754c135298a8de27b21
Judy Wu <judyzhaoxinwu@gmail.com>
2015-05-16 15:59:15 -0400

    Delete config.reload_classes_only_on_change = false in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,6 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.reload_classes_only_on_change = false
-
   config.to_prepare do
     Thread.current.keys.each{ |k| Thread.current[k] = nil if k.to_s =~ /_scoped_methods$/ }
   end
