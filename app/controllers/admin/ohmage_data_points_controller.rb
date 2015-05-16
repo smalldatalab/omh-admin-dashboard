20d5218e164ff7a0f5e5cb213b55e20aa82e0300
Judy Wu <judyzhaoxinwu@gmail.com>
2015-05-16 15:00:43 -0400

    Change action: 'show' to partial: 'show' staging deploy

--- app/controllers/admin/ohmage_data_points_controller.rb
+++ app/controllers/admin/ohmage_data_points_controller.rb
@@ -3,7 +3,7 @@ class Admin::OhmageDataPointsController < ApplicationController
     @user = User.find(params[:user_id])
     respond_to do |format|
       format.csv {render text: @user.ohmage_data_csv(current_admin_user.id)}
-      format.html {render action: 'show', method: @user.calendar_ohmage_events_array(current_admin_user.id)}
+      format.html {render partial: 'show', method: @user.calendar_ohmage_events_array(current_admin_user.id)}
     end
   end
 end
--- app/controllers/admin/pam_data_points_controller.rb
+++ app/controllers/admin/pam_data_points_controller.rb
@@ -3,7 +3,7 @@ class Admin::PamDataPointsController < ApplicationController
      @user = User.find(params[:user_id])
      respond_to do |format|
         format.csv {render text: @user.pam_data_csv }
-        format.html {render action: 'show', method: @user.calendar_pam_events_array}
+        format.html {render partial: 'show', method: @user.calendar_pam_events_array}
      end
   end
 end
