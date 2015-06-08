4a276fc8d22178b7f42bde84140bec80be150606
Judy Wu <judyzhaoxinwu@gmail.com>
2015-06-08 12:07:56 -0400

    Comment out the active mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
-class ApplicationMailer < ActiveMailer::Base
-  # default "from@gmail.com"
-  # layout 'mailer'
-end
+# class ApplicationMailer < ActiveMailer::Base
+#   # default "from@gmail.com"
+#   # layout 'mailer'
+# end
