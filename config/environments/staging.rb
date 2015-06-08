f1a20acd5e996d4b39ee9def3bd1fa3085323de5
Judy Wu <judyzhaoxinwu@gmail.com>
2015-06-08 12:05:16 -0400

    Testing for staging sender

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'http://lifestreams.smalldata.io'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:                 'smtp.sendgrid.net',
+    address:                 'smtp.gmail.com',
     port:                     587,
     domain:                  'gmail',
     user_name:               'jaoshinu@gmail.com',
