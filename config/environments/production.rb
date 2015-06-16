2edf6eef5b330f2578509d63773a04ec1b07cc83
Judy Wu <judyzhaoxinwu@gmail.com>
2015-06-16 12:44:58 -0400

    Debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {:host => 'http://ohmage-omh.smalldata.io'}
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address:                 'smtp.mandrillapp.com',
     port:                     587,
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {:host => 'http://lifestreams.smalldata.io'}
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address:                 'smtp.mandrillapp.com',
     port:                     587,
