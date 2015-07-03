7e22feac8fb21c1f1bb4251f0779a34637e41e25
Judy Wu <judyzhaoxinwu@gmail.com>
2015-07-03 14:59:45 -0400

    Debugging

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
-class Admin::UsersController < ApplicationController
-  # def batch_action(ids, data_stream)
-  #   respond_to do |format|
-  #     format.html {render text: multi_users_data(ids, data_stream)}
-  #   end
-  # end
-end
+# class Admin::UsersController < ApplicationController
+#   # def batch_action(ids, data_stream)
+#   #   respond_to do |format|
+#   #     format.html {render text: multi_users_data(ids, data_stream)}
+#   #   end
+#   # end
+# end
