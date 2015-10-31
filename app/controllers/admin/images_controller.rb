ccc7a04a57a98a7b61463cf2fc974c540d5add0e
Judy Wu <judyzhaoxinwu@gmail.com>
2015-10-31 12:12:24 -0400

    Test the image download function on production server

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,6 +1,12 @@
 class Admin::ImagesController < ApplicationController
   def show
     @image = Mongoid::GridFs.get(params[:id])
-    send_data @image.data, type: @image.content_type, disposition: 'inline'
+    @filename = @image.filename
+    @temp = Tempfile.new(params[:id])
+    @data = File.read(@filename)
+    File.open(params[:id], 'wb') do |f|
+      f.write(@data)
+    end
+    send_file @image.path, type: @image.content_type, disposition: 'attachment'
   end
 end
