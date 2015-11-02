c2170f3c0cee089d1058804cc752be370332f5c2
Judy Wu <judyzhaoxinwu@gmail.com>
2015-11-02 12:17:46 -0500

    Remove spawn 'mongofiles -d omh get ' + @filename

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -2,12 +2,10 @@ class Admin::ImagesController < ApplicationController
   def show
     @image = Mongoid::GridFs.get(params[:id])
     @filename = @image.filename
-    spawn 'mongofiles -d omh get ' + @filename
 
     @temp = Tempfile.new(params[:id])
     @data = File.read(@filename)
-    spawn 'rm -Rf ' + @filename
-
+    # spawn 'rm -Rf ' + @filename
     begin
       File.open(params[:id], 'wb') do |f|
         f.write(@data)
