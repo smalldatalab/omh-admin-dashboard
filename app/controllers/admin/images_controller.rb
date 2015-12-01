eef84d1396ed1d2ffdd30f9674ec25a917b6f7ad
Judy Wu <judyzhaoxinwu@gmail.com>
2015-12-01 13:00:19 -0500

    Debug for file name

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -2,20 +2,22 @@ class Admin::ImagesController < ApplicationController
   def show
     image = Mongoid::GridFs.get(params[:id])
     filename = image.filename
-    save_path = '~/admindashboard/current/data'
+    # save_path = '~/admindashboard/current/data'
     file_path = File.join(Rails.root.to_s + '/data/' + filename)
 
-    temp = Tempfile.new(params[:id])
+    # temp = Tempfile.new(params[:id])
     data = File.read(file_path)
     # spawn 'rm -Rf ' + @filename
-    begin
-      File.open(params[:id], 'wb') do |f|
-        f.write(data)
-      end
-      send_file file_path, type: image.content_type, disposition: 'attachment'
-    ensure
-      temp.close
-      temp.unlink
-    end
+
+    send_file file_path, type: image.content_type, disposition: 'attachment'
+    # begin
+    #   File.open(params[:id], 'wb') do |f|
+    #     f.write(data)
+    #   end
+    #   send_file file_path, type: image.content_type, disposition: 'attachment'
+    # ensure
+    #   temp.close
+    #   temp.unlink
+    # end
   end
 end
