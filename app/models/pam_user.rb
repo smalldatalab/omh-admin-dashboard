fad4afaf55b4d601b600a2a2cf2e4231fcb381e0
Judy Wu <judyzhaoxinwu@gmail.com>
2015-12-05 21:28:44 -0500

    Debug for mongdb

--- app/models/pam_user.rb
+++ app/models/pam_user.rb
@@ -3,9 +3,9 @@ class PamUser
   store_in collection: 'endUser', database: 'omh'
 
   field :_id, type: Object
-  field :_class, type: String
-  field :password_hash, type: Hash
-  field :registration_timestamp, type: Time
+  # field :_class, type: String
+  # field :password_hash, type: Hash
+  # field :registration_timestamp, type: Time
 
   has_many :pam_data_points
 
