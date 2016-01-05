fa0a41929cdc688966a2f3f6bb2effe417ab4a1b
Jared Sieling <jared.sieling@gmail.com>
2016-01-05 08:30:38 -0600

    Change data URLs to relative.

--- app/assets/javascripts/dsu.js
+++ app/assets/javascripts/dsu.js
@@ -1,4 +1,4 @@
-var dsu = "https://aws-qa.smalldata.io/dsu/";
+var dsu = window.location.origin + "/dsu/";
 
 function getDatapointId(username, date, device){
     return  ["mobility-daily-segments", username, date, device].join("-");
