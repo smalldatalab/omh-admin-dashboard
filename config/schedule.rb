df87c0648b522bfd6777394ff128c3d01d35b00b
Judy Wu <judyzhaoxinwu@gmail.com>
2016-01-10 14:16:46 -0500

    Cron job edited

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,5 @@
 set :output, Whenever.path + "/log/cron_job.log"
 
-every '0,5,10,15,20,25,30,35,40,45,50,55 * * * *' do
-  command "echo 'two'"
-end
-
 every :day, :at => '12:20am' do
   command "find " +  Whenever.path + "/data -type f -mtime +1 -exec rm {} +"
 end
